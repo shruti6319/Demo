8490a7a1e549a6aed6822c18e027912d4c2049ea
shruti6319 <shrutithorat7602@gmail.com>
2026-05-11 13:22:38 +0530

    changes from conflict demo

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        //hii
+        System.out.println("code from conflict branch");
 
     }
 }
